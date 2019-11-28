9759731497e81d8491eeaaa8fc2cd86dae044aaf
Dejan Sandic <dejansandic13@gmail.com>
2019-11-28 22:25:10 +0100

    Fix linting issues

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,7 @@
 import CleanReact from '../src/index';
 
 describe('main test', () => {
-	it('should not brake', () => {
-		expect(typeof CleanReact).toBe('function');
-	});
+   it('should not brake', () => {
+      expect(typeof CleanReact).toBe('function');
+   });
 });
